a5b024d29010e069beb8348a50d30e7731633202
Omega Dziko F. <megaoy@yahoo.com>
2025-01-10 13:25:17 -0500

    stylings

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from .config import Config
+from random import choice
+from .tweets import tweets
+
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+@app.route("/")
+def index():
+    random_tweet = choice(tweets)
+    return render_template("index.html", tweet=random_tweet)
+
+@app.route("/feed")
+def feed():
+    return render_template("feed.html", tweets=tweets)
